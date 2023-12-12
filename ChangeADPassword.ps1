b00c57d0661811fdca4f09eec280fac631d451c9
AbhishekKumar <103169433+1997Abhishekuipath@users.noreply.github.com>
2023-12-12 15:36:32 +0530

    powershellfile

---
+++ ChangeADPassword.ps1
@@ -0,0 +1,17 @@
+﻿param (
+    [string]$ADadminuser,
+    [string]$adminpassword,
+    [string]$user,
+    [string]$domainName,
+    [string]$randomPassword
+)
+
+Write-Host "ADadminuser: $ADadminuser"
+Write-Host "adminpassword: $adminpassword"
+Write-Host "user: $user"
+Write-Host "domainName: $domainName"
+Write-Host "randomPassword: $randomPassword"
+
+$password = ConvertTo-SecureString $adminpassword -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential($ADadminuser, $password)
+Set-ADAccountPassword -Server $domainName -Credential $cred -Identity $user -Reset -NewPassword (ConvertTo-SecureString -AsPlainText $randomPassword -Force)
